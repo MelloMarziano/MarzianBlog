258d46fb49239efd71817a1a61653612bbc961c9
Eliu Ortega <eliu.orteg@gmail.com>
2016-05-04 13:51:35 -0400

    Cambiada la conexion

--- conexion/conexion.php
+++ conexion/conexion.php
@@ -1,9 +1,9 @@
 <?php 
 
-	$host="185.27.134.10";
-	$user="n260m_17081553";
-	$pw="jpkhf164";
-	$db="n260m_17081553_blog";
+	$host="127.0.0.1";
+	$user="root";
+	$pw="";
+	$db="blog";
 
 	$con =new mysqli($host,$user,$pw,$db);
 	if ($con->connect_errno){
